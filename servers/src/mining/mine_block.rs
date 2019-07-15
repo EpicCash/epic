f210ce4fdb414e9ea27871a12dcdf7763a5f0667
Aron Castro <aron@brickabode.com>
2019-07-15 13:59:59 -0300

    Add foundation output in miner build block

--- servers/src/mining/mine_block.rs
+++ servers/src/mining/mine_block.rs
@@ -31,6 +31,7 @@ use crate::core::core::{Output, TxKernel};
 use crate::core::global::get_policies;
 use crate::core::libtx::secp_ser;
 use crate::core::{consensus, core, global};
+use crate::core::core::foundation::load_foundation_output;
 use crate::keychain::{ExtKeychain, Identifier, Keychain};
 pub use crate::core::core::foundation::CbData;
 use crate::pool;
@@ -176,8 +177,10 @@ fn build_block(
 		height,
 	};
 
+	let cb_data = load_foundation_output(height);
+
 	let (output, kernel, block_fees) = get_coinbase(wallet_listener_url, block_fees, height)?;
-	let mut b = core::Block::from_reward(&head, txs, output, kernel, difficulty.difficulty)?;
+	let mut b = core::Block::from_coinbases(&head, txs, (output, kernel), (cb_data.output, cb_data.kernel), difficulty.difficulty)?;
 
 	// making sure we're not spending time mining a useless block
 	b.validate(&head.total_kernel_offset, verifier_cache)?;
