14e9bd10c756d1f1babdf30f3445484d70f6b8d4
Johannes Hahn <j.hahn@eccence.de>
2021-10-19 20:56:25 +0200

    test final windows sha

--- core/src/global.rs
+++ core/src/global.rs
@@ -94,6 +94,10 @@ pub const COMPACTION_CHECK: u64 = DAY_HEIGHT;
 
 pub const CURRENT_HEADER_VERSION: u16 = 6;
 
+#[cfg(target_family = "unix")]
+pub const FOUNDATION_JSON_SHA256: &str =
+	"2613717d04128587b8c3fa24db873b2c4b33232cffe1849b4b5e1ff6d39cd12d";
+#[cfg(target_family = "windows")]
 pub const FOUNDATION_JSON_SHA256: &str =
 	"2613717d04128587b8c3fa24db873b2c4b33232cffe1849b4b5e1ff6d39cd12d";
 
