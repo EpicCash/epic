7ac26bae687606cc7f16c4a3e26a7e589b14bbe4
JhelisonUchoa <jhelison@brickabode.com>
2022-12-06 20:54:54 -0300

    docs: Add readme to the release path

--- etc/build/build-release-windows.sh
+++ etc/build/build-release-windows.sh
@@ -1,28 +1,23 @@
 #!/usr/bin/env bash
 
-# Code used to build the releases for the project
-# The final output is the tar.gz file and the checksum with sha256 for macos
+# As a semi-automatic approach, this sh can generate the final zip and sha256 for windows builds
+# This requires that you build the exe binary and place it on a release folder from the root of the project
 # This must be executed from the root of the project
 
-# Initialize the project variables
 NAME=$(awk -F "=" '/name/ {print $2}' Cargo.toml | head -1 | tr -d '"' | tr -d ' ')
 VERSION=$(awk -F "=" '/version/ {print $2}' Cargo.toml | head -1 | tr -d '"' | tr -d ' ')
-OS=macos
+OS=windows
 
 OUTPUTPATH=release
-BINARY=./target/release/epic
-TARFILE=$NAME-$VERSION-$OS.tar.gz
+BINARY=./release/epic.exe
+ZIPFILE=$NAME-$VERSION-$OS.zip
 SHAFILE=$NAME-$VERSION-$OS-sha256sum.txt
-
-# Clean the Build the project
-cargo clean
-cargo build --release
 
 # Create the output path and tar the files and dependencies
 mkdir -p ./$OUTPUTPATH/$NAME
 cp ./debian/foundation.json ./etc/README.MD $BINARY ./$OUTPUTPATH/$NAME
-cd ./$OUTPUTPATH && tar -czvf $TARFILE epic/ && cd ../
+cd ./$OUTPUTPATH && zip $ZIPFILE epic/* && cd ../
 rm -r ./$OUTPUTPATH/$NAME/
 
 # Generate the sha256sum
-shasum -a 256 ./$OUTPUTPATH/$TARFILE | sed 's, .*/, ,' > ./$OUTPUTPATH/$SHAFILE
+shasum -a 256 ./$OUTPUTPATH/$ZIPFILE | sed 's, .*/, ,' > ./$OUTPUTPATH/$SHAFILE
