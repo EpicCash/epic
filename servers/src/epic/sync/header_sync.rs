c5a010ef2e96c9477d6da7d74f716394051154b3
who-biz <37732338+who-biz@users.noreply.github.com>
2023-06-08 18:07:57 +0000

    Remove logging/breakpoints from header sync

--- servers/src/epic/sync/header_sync.rs
+++ servers/src/epic/sync/header_sync.rs
@@ -64,49 +64,18 @@ impl HeaderSync {
 	pub fn check_run(&mut self) -> Result<(Vec<BlockHeader>, bool), chain::Error> {
 		let mut peer_blocks = false;
 
-		trace!(
-			"check_run: now ({}) start_time ({}) diff({})",
-			Utc::now().timestamp(),
-			self.start_time,
-			(Utc::now().timestamp() - self.start_time)
-		);
-
 		match self.peers.get_connected_peer(self.peer.info.addr) {
 			Some(peer) => {
-				let now = Utc::now().timestamp();
-				trace!(
-					"check_run, peermatch: now ({}) start_time ({}) diff({})",
-					now,
-					self.start_time,
-					(now - self.start_time)
-				);
-				if !peer.is_connected() || peer.is_banned()
-				/*|| !peer.info.capabilities.contains(p2p::types::Capabilities::HEADER_FASTSYNC)*/
-				{
+				if !peer.is_connected() || peer.is_banned() {
 					peer_blocks = true;
 				}
 			}
 			None => {
-				let now = Utc::now().timestamp();
-				trace!(
-					"check_run, peermatchnone: now ({}) start_time ({}) diff({})",
-					now,
-					self.start_time,
-					(now - self.start_time)
-				);
 				peer_blocks = true;
 			}
 		}
 
 		if !self.syncing_peer {
-			let now = Utc::now().timestamp();
-			trace!(
-				"check_run,syncing_peer: now ({}) start_time ({}) diff({})",
-				now,
-				self.start_time,
-				(now - self.start_time)
-			);
-
 			info!(
 				"{:?}\tnew sync peer, offset: {:?}",
 				self.peer.info.addr, self.offset
@@ -117,13 +86,6 @@ impl HeaderSync {
 				highest_height: self.highest_height,
 			});
 
-			let now = Utc::now().timestamp();
-			trace!(
-				"check_run, after sync_state.update: now ({}) start_time ({}) diff({})",
-				now,
-				self.start_time,
-				(now - self.start_time)
-			);
 			self.syncing_peer = true;
 
 			//reset previous queued headers
@@ -138,13 +100,6 @@ impl HeaderSync {
 
 	fn header_sync_due(&mut self) -> bool {
 		let now = Utc::now().timestamp();
-
-		info!(
-			"header_sync_due: time (now = {}, start_time = {}, diff {}",
-			now,
-			self.start_time,
-			(now - self.start_time)
-		);
 
 		if (now - self.start_time) > 120 {
 			let _ = self
@@ -173,14 +128,6 @@ impl HeaderSync {
 
 	/// Request some block headers from a peer to advance us.
 	fn request_headers_fastsync(&mut self) {
-		let now = Utc::now().timestamp();
-		trace!(
-			"request_headers_fastsync: now ({}) start_time ({}) diff({})",
-			now,
-			self.start_time,
-			(now - self.start_time)
-		);
-
 		if let Ok(locator) = self.get_locator() {
 			if self.offset == 0
 				&& !self
@@ -190,14 +137,13 @@ impl HeaderSync {
 					.contains(p2p::types::Capabilities::HEADER_FASTSYNC)
 			{
 				info!(
-					">>>> slow sync: request headers: asking {} for headers, {:?}, offset {:?}",
+					"sync: request slowsync headers: asking {} for headers, {:?}, offset {:?}",
 					self.peer.info.addr, locator, self.offset
 				);
-				//debug!(">>> slow sync peer, do nothing");
 				let _ = self.peer.send_header_request(locator);
 			} else {
 				info!(
-					"<<<< fast sync: request fastsync headers: asking {} for headers, {:?}, offset {:?}",
+					"sync: request fastsync headers: asking {} for headers, {:?}, offset {:?}",
 					self.peer.info.addr, locator, self.offset
 				);
 				let _ = self.peer.send_header_fastsync_request(locator, self.offset);
@@ -209,18 +155,8 @@ impl HeaderSync {
 	/// Even if sync_head is significantly out of date we will "reset" it once we
 	/// start getting headers back from a peer.
 	fn get_locator(&mut self) -> Result<Vec<Hash>, Error> {
-		let mut iter = 0;
-		let now = Utc::now().timestamp();
-		trace!(
-			"get_locator, start: now ({}) start_time ({}) diff({})",
-			now,
-			self.start_time,
-			(now - self.start_time)
-		);
-
 		let tip = self.chain.get_sync_head()?;
 		let heights = get_locator_heights(tip.height);
-		warn!(">>> heights_string ({:?})", heights);
 
 		// for security, clear history_locator[] in any case of header chain rollback,
 		// the easiest way is to check whether the sync head and the header head are identical.
@@ -228,43 +164,17 @@ impl HeaderSync {
 			self.history_locator.retain(|&x| x.0 == 0);
 		}
 
-		let now = Utc::now().timestamp();
-		trace!(
-			"get_locator, bp1: now ({}) start_time ({}) diff({})",
-			now,
-			self.start_time,
-			(now - self.start_time)
-		);
-
 		// for each height we need, we either check if something is close enough from
 		// last locator, or go to the db
 		let mut locator: Vec<(u64, Hash)> = vec![(tip.height, tip.last_block_h)];
 		for h in heights {
 			if let Some(l) = close_enough(&self.history_locator, h) {
-				let now = Utc::now().timestamp();
-				trace!(
-					"get_locator, bp2: now ({}) start_time ({}) diff({})",
-					now,
-					self.start_time,
-					(now - self.start_time)
-				);
 				locator.push(l);
 			} else {
 				// start at last known hash and go backward
 				let last_loc = locator.last().unwrap().clone();
 				let mut header_cursor = self.chain.get_block_header(&last_loc.1);
 				while let Ok(header) = header_cursor {
-					iter += 1;
-					let now = Utc::now().timestamp();
-					warn!(
-						"get_locator, bp3: now ({}) start_time ({}) diff({}) header.height({}), h({}), iter({})",
-						now,
-						self.start_time,
-						(now-self.start_time),
-						header.height,
-						h,
-						iter
-					);
 					if header.height == h {
 						if header.height != last_loc.0 {
 							locator.push((header.height, header.hash()));
@@ -275,25 +185,10 @@ impl HeaderSync {
 				}
 			}
 		}
-		let now = Utc::now().timestamp();
-		trace!(
-			"get_locator, bp4: now ({}) start_time ({}) diff({})",
-			now,
-			self.start_time,
-			(now - self.start_time)
-		);
 
 		locator.dedup_by(|a, b| a.0 == b.0);
 		debug!("sync: locator : {:?}", locator.clone());
 		self.history_locator = locator.clone();
-
-		let now = Utc::now().timestamp();
-		warn!(
-			"get_locator, end: now ({}) start_time ({}) diff({})",
-			now,
-			self.start_time,
-			(now - self.start_time)
-		);
 
 		Ok(locator.iter().map(|l| l.1).collect())
 	}
