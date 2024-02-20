3490e81604ff4e49282b54f65106ed17476bbc69
who-biz <37732338+who-biz@users.noreply.github.com>
2024-02-20 15:48:47 -0500

    Update libgit2-sys, resolve dependabot vulnerability

--- src/build/build.rs
+++ src/build/build.rs
@@ -17,6 +17,7 @@
 use built;
 
 use std::env;
+use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
@@ -39,13 +40,9 @@ fn main() {
 			.expect("failed to execute git config for hooks");
 	}
 
-	// build and versioning information
-	let mut opts = built::Options::default();
-	opts.set_dependencies(true);
 	// don't fail the build if something's missing, may just be cargo release
 	let _ = built::write_built_file_with_opts(
-		&opts,
-		env!("CARGO_MANIFEST_DIR"),
-		format!("{}{}", env::var("OUT_DIR").unwrap(), "/built.rs"),
+		Some(Path::new(env!("CARGO_MANIFEST_DIR"))),
+		&Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs"),
 	);
 }
