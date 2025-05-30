4f7be0c54fc446d3159e6f0d11225ff8af5828cd
Johannes Hahn <mail@johanneshahn.online>
2025-05-30 10:12:42 +0200

    fix: add logging for existing peer capabilities and flags during connection

--- p2p/src/peers.rs
+++ p2p/src/peers.rs
@@ -68,6 +68,11 @@ impl Peers {
 		let mut ban_reason = ReasonForBan::None;
 
 		if let Ok(existing) = self.get_peer(peer.info.addr) {
+			warn!(
+				"add_connected: Addr {}. Capabilities: {:?}, User Agent: {}, Flags: {:?}",
+				existing.addr, existing.capabilities, existing.user_agent, existing.flags
+			);
+
 			if existing.flags == State::Banned {
 				flags = State::Banned;
 				last_banned = existing.last_banned;
